7e59d2ba6cacfb51a9fe3646bc44035393e3362d
P S BHARATH KUMAR ACHARI <bharaths_mac@Ps-Laptop.local>
2024-05-07 09:12:13 +0530

    routers_added

--- faculty_Verifier/migrations/1_deploy.js
+++ faculty_Verifier/migrations/1_deploy.js
@@ -2,7 +2,8 @@
 const FacultyVerifier = artifacts.require("FacultyVerifier");
 const FacultyRegistry = artifacts.require("FacultyRegistry");
 
+
 module.exports = function (deployer) {
-  const facultyRegistryAddress = "0xBEAB6A2E10F5AFb1782427c08887062Dbb9904d3"; // Address of the FacultyRegistry contract
+  const facultyRegistryAddress = "0xd3fc4A74F7eF23f993eae7788dfE16a735208Bed"; // Address of the FacultyRegistry contract
   deployer.deploy(FacultyVerifier, facultyRegistryAddress);
 };
--- recordAttendance/migrations/1_RecordAttendanceOfStudent.js
+++ recordAttendance/migrations/1_RecordAttendanceOfStudent.js
@@ -2,5 +2,5 @@ const RecordAttendanceOfStudent = artifacts.require("RecordAttendanceOfStudent")
 
 module.exports = function (deployer) {
   // Deploy RecordAttendanceOfStudent contract with the FacultyTimetable contract address
-  deployer.deploy(RecordAttendanceOfStudent, "0x3b34c783651f0832E8977e1e444D2fd172034552");
+  deployer.deploy(RecordAttendanceOfStudent, "0x3Fef5cc50B970ae002e1f6CeB657379Ac4fd4a40");
 };
